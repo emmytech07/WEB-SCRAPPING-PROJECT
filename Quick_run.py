074dfe362ee7fa10c73195c57a7a34326645d267
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-09-22 04:05:22 +0100

    checking

--- Quick_run.py
+++ Quick_run.py
@@ -1,6 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
 
 driver = webdriver.Chrome('./chromedriver')
 driver.get("https://www.python.org")
 print(driver.title)
+
+# find element by name 
+Name_search = driver.find_element(By.CSS_SELECTOR, value="input[name='q']")
+Name_search.clear
+Name_search.send_keys("checking")
+Name_search.send_keys(Keys.RETURN)
