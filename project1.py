c5bab7444736436d90885fcb207c5b073c173fa0
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-09-22 02:40:17 +0100

    updated

--- project1.py
+++ project1.py
@@ -1,9 +1,16 @@
 # https://www.thesun.co.uk/sport/football/
 
+
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+# from selenium.webdriver.chrome.service import Service
 
 website = "https://www.thesun.co.uk/sport/football/"
-path = "C:\chromedriver"
+# path = "/Users/hp/Downloads/"
 
-service = Service(executable_path=path)
+# service = Service(executable_path=path)
+driver = webdriver.Chrome(ChromeDriverManager().install())
+# driver = webdriver.Chrome(executable_path="/Users/hp/Downloads/")
+
+driver.get(website)
