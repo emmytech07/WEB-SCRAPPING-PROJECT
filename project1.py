8cc20fffbf7e5dfbc86a9a728ff78b04a412cb20
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-09-22 03:16:47 +0100

    getting content

--- project1.py
+++ project1.py
@@ -14,3 +14,9 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 # driver = webdriver.Chrome(executable_path="/Users/hp/Downloads/")
 
 driver.get(website)
+containers = driver.find_elements(by="xpath", value="//div[@class='teaser__copy-container']")
+for container in containers:
+    title = container.find_element(by="xpath", value='./a/h2').text()
+    subtitle = container.find_element(by="xpath", value='./a/p').text()
+
+    link = container.find_element(by='xpath', value='./a').get_attribute("href")
