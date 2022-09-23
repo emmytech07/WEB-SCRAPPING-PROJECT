59f521529b0b9cbed9982b6f19a6f7c4c8f905ad
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-09-23 03:14:09 +0100

    improving the code

--- Quick_run.py
+++ Quick_run.py
@@ -8,7 +8,13 @@ driver.get("https://www.python.org")
 print(driver.title)
 
 # find element by name 
-Name_search = driver.find_element(By.CSS_SELECTOR, value="input[name='q']")
+# Name_search = driver.find_element(By.CSS_SELECTOR, value="input[name='q']")
+# Name_search = driver.find_element(By.ID, value="id-search-field")
+# Name_search = driver.find_element(By.CLASS_NAME, value="search-field")
+Name_search = driver.find_element(By.XPATH, value="//input[@id='id-search-field']")
 Name_search.clear
 Name_search.send_keys("checking")
 Name_search.send_keys(Keys.RETURN)
+print(driver.current_url)
+driver.switch_to_window('window_name')
+driver.close()
